3bb8809e94caff729b41bd7cae322841bd6ec780
Frederik Aust <frederik@studio-aust.com>
2014-11-19 19:36:05 +0100

    First draft of Word reference file; some hacks to make apa.table() work in word. They ain't pretty.

--- helper/apatable.r
+++ helper/apatable.r
@@ -1,20 +1,83 @@
-apa.table <- function(x, caption = NULL, note = NULL, placement = "tbp", landscape = FALSE, ...) {
+apa.table <- function(output_format = NULL, ...) {
   require("knitr")
+  if(output_format == "latex") {
+    apa.table.latex(...)
+  } else {
+    apa.table.word(...)
+  }
+}
+
+apa.table.latex <- function(x, caption = NULL, note = NULL, placement = "tbp", landscape = FALSE, merge.names = NULL, ...) {
   if(landscape) table_env <- "sidewaystable" else table_env <- "table"
   cat("\\begin{", table_env, "}[", placement, "]\n\\centering\n\\begin{threeparttable}\n\\caption{", caption, "}", sep = "")
-
+  
   if(is.list(x) && !is.data.frame(x)) {
-    lapply(seq_along(x), function(i) {
-        cat("\n\\begin{tabular}{@{}c@{}}\n")
-        if(i == 1) cat("\\toprule\n")
-        cat(names(x)[i], "\\\\\n")
-        kable(x[[i]], format = "latex", booktabs = TRUE, ...)
-        cat("\\\\\n\\end{tabular}")
+    tables_to_merge <- names(x)
+    prep_table <- lapply(seq_along(x), function(i) {
+      prep_table <- cbind(
+        c(tables_to_merge[i], rep("", nrow(x[[i]])-1))
+        , rownames(x[[i]])
+        , x[[i]]
+      )
+      rownames(prep_table) <- NULL
+      second_col <- ifelse(is.null(rownames(x[[i]])), NULL, "")
+      if(is.null(merge.names)) {
+        colnames(prep_table) <- c("", "", colnames(x[[i]]))
+      } else {
+        colnames(prep_table) <- c(merge.names, colnames(x[[i]]))
       }
+      
+      return(prep_table)
+    }
     )
+    x_merged <- do.call(rbind, prep_table)
+    kable(x_merged, format = "latex", booktabs = TRUE, ...)
   } else kable(x, format = "latex", booktabs = TRUE, ...)
-
+  
   cat("\n")
   if(!is.null(note)) cat("\\tablenotes{\\textit{Note.}", note, "}\n")
   cat("\\end{threeparttable}\n\\end{", table_env, "}", sep = "")
+}
+
+apa.table.word <- function(x, caption = NULL, note = NULL, merge.names = NULL, ...) {
+  if(is.list(x) && !is.data.frame(x)) {
+    tables_to_merge <- names(x)
+    prep_table <- lapply(seq_along(x), function(i) {
+      prep_table <- cbind(
+        c(tables_to_merge[i], rep("&nbsp;", nrow(x[[i]])-1)) # Without the &nbsp; empty cells are set as standard instead of compact
+        , rownames(x[[i]])
+        , x[[i]]
+      )
+      rownames(prep_table) <- NULL
+      second_col <- ifelse(is.null(rownames(x[[i]])), NULL, "")
+      if(is.null(merge.names)) {
+        colnames(prep_table) <- c("&nbsp;", "&nbsp;", colnames(x[[i]]))
+      } else {
+        colnames(prep_table) <- c(merge.names, colnames(x[[i]]))
+      }
+      
+      return(prep_table)
+    }
+    )
+    x_merged <- do.call(rbind, prep_table)
+    cat("<center>")
+    cat("Table. ")
+    cat("*", caption, "*", sep = "")
+    cat("</center>")
+    kable(x_merged, format = "pandoc", ...)
+  } else {
+    colnames(x) <- ifelse(colnames(x) == "", "&nbsp;", colnames(x))
+    prep_table <- cbind(rownames(x), x)
+    colnames(prep_table) <- c("&nbsp;", colnames(x))
+    rownames(prep_table) <- NULL
+    kable(prep_table, format = "pandoc", ...)
+  }
+  
+  if(!is.null(note)) {
+    cat("\n")
+    cat("<center>")
+    cat("*Note.*", note)
+    cat("</center>")
+    cat("\n\n\n\n")
+  }
 }
