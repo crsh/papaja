e291a76932927ce87ba0ad76fd0d68d8855849fa
Birk Diedenhofen <mail@birkdiedenhofen.de>
2015-02-03 18:09:54 +0100

    Added full stops at the end of all validation error messages.

--- R/00_input_validation.R
+++ R/00_input_validation.R
@@ -1,18 +1,18 @@
 validate.numeric <- function(x, name, type="number") {
-  if(!is.null(x) && any(is.na(x))) stop(paste("The parameter '", name, "' is NA", sep=""))
-  if(!is(x, "numeric") || any(is.infinite(x)) || length(x) != 1 || (type == "integer" && x %% 1 != 0)) stop(paste("The parameter '", name, "' must be a single ", type, sep=""))
+  if(!is.null(x) && any(is.na(x))) stop(paste("The parameter '", name, "' is NA.", sep=""))
+  if(!is(x, "numeric") || any(is.infinite(x)) || length(x) != 1 || (type == "integer" && x %% 1 != 0)) stop(paste("The parameter '", name, "' must be a single ", type, ".", sep=""))
 }
 
 validate.character <- function(x, name, vector.length=1) {
-  if(!is.null(x) && any(is.na(x))) stop(paste("The parameter '", name, "' is NA", sep=""))
-  if(!is(x, "character") || !(length(x) %in% vector.length)) stop(paste("The parameter '", name, "' must be a ", ifelse(length(vector.length) == 1 && vector.length == 1, "single", paste0("vector of ", paste(vector.length, collapse=" or "))), " character string", if(any(vector.length > 1)) "s", sep=""))
+  if(!is.null(x) && any(is.na(x))) stop(paste("The parameter '", name, "' is NA.", sep=""))
+  if(!is(x, "character") || !(length(x) %in% vector.length)) stop(paste("The parameter '", name, "' must be a ", ifelse(length(vector.length) == 1 && vector.length == 1, "single", paste0("vector of ", paste(vector.length, collapse=" or "))), " character string", if(any(vector.length > 1)) "s", ".", sep=""))
 }
 
 validate.numeric.range <- function(x, name, lower, upper) {
-  if(x < lower || x > upper) stop(paste("The parameter '", name, "' must be a single number between ", lower, " and ", upper, sep=""))
+  if(x < lower || x > upper) stop(paste("The parameter '", name, "' must be a single number between ", lower, " and ", upper, ".", sep=""))
 }
 
 validate.logical <- function(x, name) {
-  if(!is.null(x) && any(is.na(x))) stop(paste("The parameter '", name, "' is NA", sep=""))
-  if(!is(x, "logical") || length(x) != 1) stop(paste("The parameter '", name, "' must be a single logical value", sep=""))
+  if(!is.null(x) && any(is.na(x))) stop(paste("The parameter '", name, "' is NA.", sep=""))
+  if(!is(x, "logical") || length(x) != 1) stop(paste("The parameter '", name, "' must be a single logical value.", sep=""))
 }
