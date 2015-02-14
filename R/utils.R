d3c8517184ebbae20d9303703379073f58696a79
Frederik Aust <frederik@studio-aust.com>
2015-02-14 20:11:14 +0100

    Updated printnum() input validation and validate() to allow input vectors.

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,21 @@
-validate <- function(x, name, check.class=NULL, check.integer=FALSE, check.NA=TRUE, check.infinite=TRUE, check.length=NULL, check.dim=NULL, check.range=NULL) {
+validate <- function(
+  x
+  , name
+  , check.class = NULL
+  , check.integer = FALSE
+  , check.NA = TRUE
+  , check.infinite = TRUE
+  , check.length = NULL
+  , check.dim = NULL
+  , check.range = NULL
+) {
   if(is.null(x)) stop(paste("The parameter '", name, "' is NULL.", sep=""))
 
   if(!is.null(check.dim) && !all(dim(x) == check.dim)) stop(paste("The parameter '", name, "' must have dimensions " , paste(check.dim, collapse=""), ".", sep=""))
   if(!is.null(check.length) && length(x) != check.length) stop(paste("The parameter '", name, "' must be of length ", check.length, ".", sep=""))
 
-  if(is.na(x)) {
-    if(check.NA) stop(paste("The parameter '", name, "' is NA.", sep=""))
+  if(any(is.na(x))) {
+    if(check.NA) stop(paste("The parameter '", name, "' is NA.", sep = ""))
     else return(TRUE)
   }
 
