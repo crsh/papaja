99cc6e43d19d5805522adef5a1af2d3d40eca7b8
Frederik Aust <frederik@studio-aust.com>
2015-03-02 16:15:27 +0100

    Documented validate() and make_confint(); created and documented convert_stat_name(). Created unit test for utility functions.

---
+++ inst/tests/test_utils.R
@@ -0,0 +1,46 @@
+library("testthat")
+source("../../R/utils.R")
+
+context("Utility functions")
+
+test_that(
+  "convert_stat_name()"
+  , {
+    chis <- c(
+      convert_stat_name("X-squared")
+      , convert_stat_name("Chi-squared")
+      , convert_stat_name("chi-squared")
+      , convert_stat_name("X^2")
+    )
+    expect_that(chis, equals(rep("\\Chi^2", length(chis))))
+
+    expect_that(convert_stat_name("t"), equals("t"))
+    expect_that(convert_stat_name("z"), equals("z"))
+  }
+)
+
+
+test_that(
+  "make_confint()"
+  , {
+    x <- c(44.4, 45.9, 41.9, 53.3, 44.7, 44.1, 50.7, 45.2, 60.1)
+    y <- c( 2.6,  3.1,  2.5,  5.0,  3.6,  4.0,  5.2,  2.8,  3.8)
+
+    cor_test <- cor.test(x, y)
+    apa_confint <- make_confint(cor_test$conf.int)
+    expect_that(apa_confint, is_a("character"))
+    expect_that(apa_confint, equals("95% CI $[-0.15$, $0.90]$"))
+
+    apa_confint <- make_confint(cor_test$conf.int, gt1 = FALSE)
+    expect_that(apa_confint, equals("95% CI $[-.15$, $.90]$"))
+
+    conf_int <- confint(lm(x ~ y))
+    apa_confint <- make_confint(conf_int)
+
+    expect_that(apa_confint, is_a("list"))
+    expect_that(length(apa_confint), equals(nrow(conf_int)))
+    expect_that(names(apa_confint), equals(c("Intercept", "y")))
+    expect_that(apa_confint$Intercept, equals("95% CI $[19.52$, $51.78]$"))
+    expect_that(apa_confint$y, equals("95% CI $[-0.95$, $7.67]$"))
+  }
+)
