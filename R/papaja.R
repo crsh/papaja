33c835be5fb7115e015550b7a33400181120d353
Frederik Aust <frederik@studio-aust.com>
2015-03-03 11:11:15 +0100

    Added template for package description.

---
+++ R/papaja.R
@@ -0,0 +1,11 @@
+#' APA manscripts from RMarkdown
+#'
+#' Papaja provides document foramts and helper functions to produces complete APA manscripts
+#' from RMarkdown-files (PDF and Word documents).
+#'
+#' ...
+#'
+#' @docType package
+#' @name papaja
+
+NULL
