5ef19077d3c6eed87bd56050354004a7e2201d01
Frederik Aust <frederik.aust@uni-koeln.de>
2015-03-06 19:03:22 +0100

    Added spaces to apa_print.aov and related methods for better reading.

--- R/anova.R
+++ R/anova.R
@@ -22,12 +22,12 @@
 #' NULL
 #' @export
 
-apa_print.aovlist <- function(x, ...){
-  x<-lapply(summary(x),arrange_summary.aov)
-  df<-do.call("rbind",x)
-  df<-data.frame(df,row.names=NULL)
+apa_print.aovlist <- function(x, ...) {
+  x <- lapply(summary(x), arrange_summary.aov)
+  df <- do.call("rbind", x)
+  df <- data.frame(df, row.names = NULL)
   values <- .anova(df, ...)
-  return(values)
+  values
 }
 
 
@@ -35,44 +35,44 @@ apa_print.aovlist <- function(x, ...){
 #' @method apa_print summary.aov
 #' @export apa_print summary.aov
 
-apa_print.summary.aov <- function(x, ...){
-  df<-arrange_summary.aov(x)
+apa_print.summary.aov <- function(x, ...) {
+  df <- arrange_summary.aov(x)
   values <- .anova(df)
-  return(values)
+  values
 }
 
-apa_print.anova <- function(x, ...){
-  df<-arrange_anova(x)
+apa_print.anova <- function(x, ...) {
+  df <- arrange_anova(x)
   values <- .anova(df, ...)
-  return(values)
+  values
 }
 
-apa_print.aov <- function(x, ...){
-  df<-arrange_aov(x)
-  values <- .anova(x=df, ...)
-  return(values)
+apa_print.aov <- function(x, ...) {
+  df <- arrange_aov(x)
+  values <- .anova(df, ...)
+  values
 }
 
-apa_print.Anova.mlm <- function(x, correction="GG", ...){
+apa_print.Anova.mlm <- function(x, correction = "GG", ...) {
 
   x <- car::summary(x)
   x$sphericity.tests
-  tmp <- x[["univariate.tests"]]
+  tmp <- x$univariate.tests
   class(tmp) <- NULL
   t.out <- data.frame(tmp)
   colnames(t.out) <- colnames(tmp)
 
-  if(nrow(x[["sphericity.tests"]])>0){
+  if(nrow(x$sphericity.tests) > 0) {
     if (correction[1] == "GG") {
-      t.out[row.names(x[["pval.adjustments"]]), "num Df"] <- t.out[row.names(x[["pval.adjustments"]]), "num Df"] * x[["pval.adjustments"]][,"GG eps"]
-      t.out[row.names(x[["pval.adjustments"]]), "den Df"] <- t.out[row.names(x[["pval.adjustments"]]), "den Df"] * x[["pval.adjustments"]][,"GG eps"]
-      t.out[row.names(x[["pval.adjustments"]]), "Pr(>F)"] <- x[["pval.adjustments"]][,"Pr(>F[GG])"]
+      t.out[row.names(x$pval.adjustments), "num Df"] <- t.out[row.names(x$pval.adjustments), "num Df"] * x$pval.adjustments[, "GG eps"]
+      t.out[row.names(x$pval.adjustments), "den Df"] <- t.out[row.names(x$pval.adjustments), "den Df"] * x$pval.adjustments[, "GG eps"]
+      t.out[row.names(x$pval.adjustments), "Pr(>F)"] <- x$pval.adjustments[,"Pr(>F[GG])"]
     } else {
       if (correction[1] == "HF") {
-        if (any(x[["pval.adjustments"]][,"HF eps"] > 1)) warning("HF eps > 1 treated as 1")
-        t.out[row.names(x[["pval.adjustments"]]), "num Df"] <- t.out[row.names(x[["pval.adjustments"]]), "num Df"] * pmin(1, x[["pval.adjustments"]][,"HF eps"])
-        t.out[row.names(x[["pval.adjustments"]]), "den Df"] <- t.out[row.names(x[["pval.adjustments"]]), "den Df"] * pmin(1, x[["pval.adjustments"]][,"HF eps"])
-        t.out[row.names(x[["pval.adjustments"]]), "Pr(>F)"] <- x[["pval.adjustments"]][,"Pr(>F[HF])"]
+        if (any(x$pval.adjustments[,"HF eps"] > 1)) warning("HF eps > 1 treated as 1")
+        t.out[row.names(x$pval.adjustments), "num Df"] <- t.out[row.names(x$pval.adjustments), "num Df"] * pmin(1, x$pval.adjustments[, "HF eps"])
+        t.out[row.names(x$pval.adjustments), "den Df"] <- t.out[row.names(x$pval.adjustments), "den Df"] * pmin(1, x$pval.adjustments[, "HF eps"])
+        t.out[row.names(x$pval.adjustments), "Pr(>F)"] <- x$pval.adjustments[,"Pr(>F[HF])"]
       } else {
         if (correction[1] == "none") {
           TRUE
@@ -80,100 +80,103 @@ apa_print.Anova.mlm <- function(x, correction="GG", ...){
       }
     }
   }
+
   df <- as.data.frame(t.out)
+
   # obtain positons of statistics in data.frame
-  old<-c("SS","num Df","Error SS","den Df", "F", "Pr(>F)")
-  nu<-c("sumsq","df","sumsq_err","df2","statistic","p.value")
-  colnames(df)==old
+  old <- c("SS", "num Df", "Error SS", "den Df", "F", "Pr(>F)")
+  nu <- c("sumsq", "df", "sumsq_err", "df2", "statistic", "p.value")
+  colnames(df) == old
   for (i in 1:length(old)){
-    colnames(df)[colnames(df)==old[i]]<-nu[i]
+    colnames(df)[colnames(df) == old[i]] <- nu[i]
   }
-  df[["term"]]<-rownames(df)
-  df<-data.frame(df,row.names=NULL)
+
+  df$term <- rownames(df)
+  df <- data.frame(df, row.names = NULL)
   values <- .anova(df, ...)
-  return(values)
+  values
 }
 
-.anova <- function(x, observed=NULL, es="ges", op = "(", cp = ")"){
+.anova <- function(x, observed = NULL, es = "ges", op = "(", cp = ")") {
 
   # from here on every class of input object is handled the same way
 
   # calculate generalized eta squared
   # This code is as copy from afex by Henrik Singmann who said that it is basically a copy from ezANOVA by Mike Lawrence
-  if(!is.null(observed)){
-    obs <- rep(FALSE,nrow(x))
+  if(!is.null(observed)) {
+    obs <- rep(FALSE, nrow(x))
     for(i in observed){
-      if (!any(str_detect(rownames(x),str_c("\\<",i,"\\>")))) stop(str_c("Observed variable not in data: ", i))
-      obs <- obs | str_detect(rownames(x),str_c("\\<",i,"\\>"))
+      if (!any(str_detect(rownames(x),str_c("\\<", i, "\\>")))) stop(str_c("Observed variable not in data: ", i))
+      obs <- obs | str_detect(rownames(x), str_c("\\<", i, "\\>"))
     }
     obs_SSn1 <- sum(x$sumsq*obs)
     obs_SSn2 <- x$sumsq*obs
-  }else{
+  } else {
     obs_SSn1 <- 0
     obs_SSn2 <- 0
   }
-  x$ges <- x$sumsq/(x$sumsq+sum(unique(x$sumsq_err))+obs_SSn1-obs_SSn2)
+  x$ges <- x$sumsq / (x$sumsq+sum(unique(x$sumsq_err)) + obs_SSn1-obs_SSn2)
   # calculate partial eta squared
-  x$pes <- x$sumsq/(x$sumsq+x$sumsq_err)
+  x$pes <- x$sumsq / (x$sumsq+x$sumsq_err)
 
   # rounding and filling with zeros
-  x[,"statistic"]<-printnum(x[,"statistic"],digits=2,margin=2)
-  x["p.value"]<-printp(x[,"p.value"])
-  x[,c("df","df2")]<-round(x[,c("df","df2")],digits=2)
-  x[,c("ges","pes")]<-printnum(x[,c("ges","pes")],digits=3,margin=2,gt1=FALSE)
+  x[, "statistic"] <- printnum(x[, "statistic"], digits = 2, margin = 2)
+  x["p.value"] <- printp(x[, "p.value"])
+  x[, c("df", "df2")] <- round(x[, c("df","df2")], digits = 2)
+  x[, c("ges","pes")] <- printnum(x[, c("ges","pes")], digits = 3, margin = 2, gt1 = FALSE)
 
   # add 'equals' where necessary
-  eq<-(1:nrow(x))[!grepl(x[,"p.value"],pattern="<|>|=")]
-  for (i in eq){
-    x[,"p.value"][i]<-paste0("= ",x[,"p.value"][i])
+  eq <- (1:nrow(x))[!grepl(x[, "p.value"], pattern="<|>|=")]
+  for (i in eq) {
+    x[, "p.value"][i] <- paste0("= ", x[, "p.value"][i])
   }
 
   # concatenate character strings
-  x[["md.text"]]<-as.character(NA)
+  x$md.text <- as.character(NA)
 
-  for (i in 1:nrow(x)){
-    x[["md.text"]][i]<-paste0("*F*", op, x[["df"]][i], ", ", x[["df2"]][i], cp, " = ", x[["statistic"]][i], ", *p* ", x[["p.value"]][i])
-    if("ges" %in% es){
-      x[["md.text"]][i]<-paste0(x[["md.text"]][i],", $\\eta^2_G$ = ", x[["ges"]][i])
+  for (i in 1:nrow(x)) {
+    x$md.text[i] <- paste0("*F*", op, x$df[i], ", ", x$df2[i], cp, " = ", x$statistic[i], ", *p* ", x$p.value[i])
+    if("ges" %in% es) {
+      x$md.text[i] <- paste0(x$md.text[i],", $\\eta^2_G$ = ", x$ges[i])
     }
-    if("pes" %in% es){
-      x[["md.text"]][i]<-paste0(x[["md.text"]][i],", $\\eta^2_p$ = ", x[["pes"]][i])
+    if("pes" %in% es) {
+      x$md.text[i] <- paste0(x$md.text[i], ", $\\eta^2_p$ = ", x$pes[i])
     }
   }
 
   # return as named list
-  values<-as.list(x[["md.text"]])
-  names(values)<-x[["term"]]
-  return(values)
+  values <- as.list(x$md.text)
+  names(values) <- x$term
+  values
 }
 
 
 ## Helper functions
 
 ## class 'anova'
-arrange_anova<-function(anova){
-  object<-as.data.frame(anova)
-  x<-data.frame(array(NA,dim=c(nrow(object)-1,7)),row.names=NULL)
-  colnames(x)<-c("term","sumsq","df","sumsq_err","df2","statistic","p.value")
-  x[,c("sumsq","df","statistic","p.value")]<-object[-nrow(object),c("Sum Sq","Df","F value","Pr(>F)")]
-  x[["sumsq_err"]]<-object[nrow(object),"Sum Sq"]
-  x[["df2"]]<-object[nrow(object),"Df"]
-  x[["term"]]<-rownames(object)[-nrow(object)]
+arrange_anova <- function(anova) {
+  object <- as.data.frame(anova)
+  x <- data.frame(array(NA, dim = c(nrow(object)-1, 7)), row.names = NULL)
+  colnames(x) <- c("term", "sumsq", "df", "sumsq_err", "df2", "statistic", "p.value")
+  x[, c("sumsq", "df", "statistic", "p.value")] <- object[-nrow(object), c("Sum Sq", "Df", "F value", "Pr(>F)")]
+  x$sumsq_err <- object[nrow(object), "Sum Sq"]
+  x$df2 <- object[nrow(object), "Df"]
+  x$term <- rownames(object)[-nrow(object)]
   x
 }
 
 ## class 'aov'
-arrange_aov<-function(aov){
-  x<-broom::tidy(aov)
-  x[["sumsq_err"]]<-x[nrow(x),"sumsq"]
-  x[["df2"]]<-x[nrow(x),"df"]
-  x<-x[-nrow(x),]
+arrange_aov <- function(aov) {
+  x <- broom::tidy(aov)
+  x$sumsq_err <- x[nrow(x), "sumsq"]
+  x$df2 <- x[nrow(x), "df"]
+  x <- x[-nrow(x), ]
   x
 }
 
 ## class 'summary.aov'
-arrange_summary.aov<-function(aov){
-  x<-arrange_aov(aov[[1]])
+arrange_summary.aov <- function(aov) {
+  x <- arrange_aov(aov[[1]])
 }
 
 
