800b1754d1040737ba4a161554873f988213a461
mariusbarth <marius.barth@uni-koeln.de>
2015-03-30 11:02:04 +0200

    Added missing documentation to fast_aggregate

--- R/aggregate.R
+++ R/aggregate.R
@@ -7,6 +7,7 @@
 #' @param factors Character. Data is intended to be aggregated by these factors.
 #' @param dv Character. The dependent variable that is aggregated. All variables in \code{data} that contain this string will be aggregated separately.
 #' @param fun The function used for aggregation.
+#' @param na.rm Logical. Specifies if missing values will be removed. Defaults to \code{FALSE}.
 #' @details If \code{x} is a vector, \code{digits}, \code{gt1}, and \code{zero} can be vectors
 #'    according to which each element of the vector is formated. Parameters are recycled if length of \code{x}
 #'    exceeds length of the parameter vectors. If \code{x} is a matrix, the vectors specify the formating
