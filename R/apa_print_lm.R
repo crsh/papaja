32870771b9e492a5aa66f6d9feb594ff00c1e745
Frederik Aust <frederik.aust@uni-koeln.de>
2015-08-31 20:17:36 +0200

    Fixed bug in apa_print.lm(): No R2-CI when F is really small.

--- R/apa_print_lm.R
+++ R/apa_print_lm.R
@@ -107,7 +107,7 @@ apa_print.lm <- function(
   summary_x <- summary(x)
   tidy_x <- broom::tidy(x)
   tidy_x <- cbind(tidy_x, ci) # Also adds term rownames
-  rownames(tidy_x) <- sanitize_terms(rownames(tidy_x), standardized)
+  rownames(tidy_x) <- sanitize_terms(tidy_x$term, standardized)
   glance_x <- broom::glance(x)
 
   # Concatenate character strings and return as named list
@@ -119,8 +119,12 @@ apa_print.lm <- function(
     paste0("$t", op, glance_x$df.residual, cp, " = ",  printnum(y["statistic"]), "$, $p ", eq, p, "$")
   })
 
-  conf_level <- as.numeric(gsub("[^.|\\d]", "", colnames(ci), perl = TRUE))
-  conf_level <- 100 - conf_level[1] * 2
+  if(is.matrix(ci)) {  
+    conf_level <- as.numeric(gsub("[^.|\\d]", "", colnames(ci), perl = TRUE))
+    conf_level <- 100 - conf_level[1] * 2
+  } else {
+    conf_level <- 100 * ci
+  }
 
   apa_res$est <- apply(tidy_x[, -1], 1, function(y) {
     paste0(
@@ -167,7 +171,12 @@ apa_print.lm <- function(
     , conf.level = ci_conf_level / 100
   ))
 
-  apa_res$est$modelfit$r2 <- paste0("$R^2 = ", printnum(glance_x$r.squared, gt1 = FALSE, zero = FALSE), "$, ", print_confint(c(r2_ci$Lower, r2_ci$Upper), conf_level = ci_conf_level))
+  if(!any(is.na(c(r2_ci$Lower, r2_ci$Upper)))) { # MBESS::ci.pvaf can sometimes result in NA if F is really small
+    apa_res$est$modelfit$r2 <- paste0("$R^2 = ", printnum(glance_x$r.squared, gt1 = FALSE, zero = FALSE), "$, ", print_confint(c(r2_ci$Lower, r2_ci$Upper), conf_level = ci_conf_level))
+  } else {
+    apa_res$est$modelfit$r2 <- paste0("$R^2 = ", printnum(glance_x$r.squared, gt1 = FALSE, zero = FALSE), "$")
+  }
+  
   apa_res$est$modelfit$r2_adj <- paste0("$R^2_{adj} = ", printnum(glance_x$adj.r.squared, gt1 = FALSE, zero = FALSE), "$")
   apa_res$est$modelfit$aic <- paste0("$AIC = ", printnum(glance_x$AIC), "$")
   apa_res$est$modelfit$bic <- paste0("$BIC = ", printnum(glance_x$BIC), "$")
