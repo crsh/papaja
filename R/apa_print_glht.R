efe87c31fa156716b0eb53e3657bac8e69ce21ce
Frederik Aust <frederik.aust@uni-koeln.de>
2015-08-31 17:12:19 +0200

    Fix to print degrees of freedom in apa_print.summary.ref.grid().

--- R/apa_print_glht.R
+++ R/apa_print_glht.R
@@ -13,7 +13,7 @@
 #'    that produce these objects and their idiosyncracies, the produced strings may sometimes be inaccurate. If you
 #'    experience inaccuracies you may report these \href{https://github.com/crsh/papaja/issues}{here} (please include
 #'    a reproducible example in your report!).
-#'    
+#'
 #'    ADJUSTED CONFIDENCE INTERVALS
 #'
 #'    \code{stat_name} and \code{est_name} are placed in the output string and are thus passed to pandoc or LaTeX through
@@ -57,17 +57,17 @@ apa_print.summary.glht <- function(
   validate(x, check_class = "summary.glht")
   validate(ci, check_class = "numeric", check_length = 1, check_range = c(0, 1))
   validate(in_paren, check_class = "logical", check_length = 1)
-  
+
   if(in_paren) {
     op <- "["; cp <- "]"
   } else {
     op <- "("; cp <- ")"
   }
-  
+
   tidy_x <- broom::tidy(x)
   test_stat <- ifelse(x$df == 0, "z", paste0("t(", x$df, ")"))
   conf_level <- paste0(ci * 100, "\\% CI")
-  
+
   # Assamble table
   ## Add (adjusted) confidence intervall
   print_ci <- confint(x, level = ci)$confint
@@ -75,35 +75,35 @@ apa_print.summary.glht <- function(
   table_ci <- unlist(print_confint(print_ci[, -1], ...)) # Remove point estimate from matrix
   contrast_table <- cbind(estimate = tidy_x$estimate, confint = table_ci, tidy_x[, c("statistic", "p.value")])
   rownames(contrast_table) <- sanitize_terms(tidy_x$lhs)
-  
+
   ## Format numbers
   contrast_table$estimate <- printnum(contrast_table$estimate, ...)
   contrast_table$statistic <- printnum(contrast_table$statistic, digits = 2)
   contrast_table$p.value <- printp(contrast_table$p.value)
-  
+
   # Concatenate character strings and return as named list
   apa_res <- list()
-  
+
   apa_res$est <- apply(contrast_table, 1, function(y) {
     paste0("$\\Delta M = ", y["estimate"], "$, ", conf_level, " ", y["confint"])
   })
-  
+
   apa_res$stat <- apply(contrast_table, 1, function(y) {
     if(!grepl("<|>", y["p.value"])) eq <- "= " else eq <- ""
-    
+
     paste0("$", test_stat, " = ", y["statistic"], "$, $p ", eq, y["p.value"], "$")
   })
-  
+
   apa_res$full <- paste(apa_res$est, apa_res$stat, sep = ", ")
   names(apa_res$full) <- names(apa_res$est)
-  
+
   apa_res <- lapply(apa_res, as.list)
-  
+
   # Add table
   rownames(contrast_table) <- tidy_x$lhs
   colnames(contrast_table) <- c("$\\Delta M$", conf_level, paste0("$", test_stat, "$"), "$p$")
   apa_res$table <- contrast_table
-  
+
   apa_res
 }
 
@@ -132,17 +132,17 @@ apa_print.summary.ref.grid <- function(
   validate(ci, check_class = "numeric", check_length = 1, check_range = c(0, 1))
   validate(in_paren, check_class = "logical", check_length = 1)
   if(!is.null(contrast_names)) validate(contrast_names, check_class = "character")
-  
+
   if(in_paren) {
     op <- "["; cp <- "]"
   } else {
     op <- "("; cp <- ")"
   }
-  
+
   split_by <- attr(x, "by.vars")
   x <- data.frame(x)
   colnames(x) <- c("contrast", "estimate", "std.error", "df", "statistic", "p.value")
-  
+
   # Assamble table
   if(!is.null(split_by)) {
     contrast_list <- split(x, x[, split_by])
@@ -156,17 +156,17 @@ apa_print.summary.ref.grid <- function(
   } else {
     contrast_table <- x
   }
-  
+
   contrast_df <- unique(round(contrast_table$df, 2))
 
   ## Calculate confidence intervals (can't use confint() because it's a summary object)
   norm_quant <- 1 - (1 - ci) / 2
   x$ll <- x$estimate - x$std.error * qnorm(norm_quant)
   x$ul <- x$estimate + x$std.error * qnorm(norm_quant)
-  
+
   if(ci < 1) conf_level <- ci * 100
   conf_level <- paste0(conf_level, "\\% CI")
-  
+
   ## Add confindence interval
   table_ci <- unlist(print_confint(matrix(c(x$ll, x$ul), ncol = 2), margin = 2, conf_level = NULL, ...))
   contrast_table <- cbind(
@@ -175,7 +175,7 @@ apa_print.summary.ref.grid <- function(
     , contrast_table[, which(colnames(contrast_table) == "df"):ncol(contrast_table)]
   )
   rownames(contrast_table) <- if(!is.null(contrast_names)) contrast_names else contrast_table$contrast
-  
+
   contrast_table <- subset(contrast_table, select = -contrast)
 
   contrast_table$estimate <- printnum(contrast_table$estimate, ...)
@@ -184,23 +184,23 @@ apa_print.summary.ref.grid <- function(
 
   # Concatenate character strings and return as named list
   apa_res <- list()
-  
+
   apa_res$est <- apply(contrast_table, 1, function(y) {
     paste0("$\\Delta M = ", y["estimate"], "$, ", conf_level, " ", y["confint"])
   })
-  
+
   apa_res$stat <- apply(contrast_table, 1, function(y) {
     if(!grepl("<|>", y["p.value"])) eq <- "= " else eq <- ""
-    
-    paste0("$", test_stat, " = ", y["statistic"], "$, $p ", eq, y["p.value"], "$")
+
+    paste0("$t(", y["df"], ") = ", y["statistic"], "$, $p ", eq, y["p.value"], "$")
   })
-  
+
   apa_res$full <- paste(apa_res$est, apa_res$stat, sep = ", ")
   names(apa_res$full) <- names(apa_res$est)
-  
+
   apa_res <- lapply(apa_res, as.list)
-  
-  
+
+
   # Add table
   if(length(contrast_df) == 1) { # Remove df column and put df in column heading
     contrast_table <- subset(contrast_table, select = -df)
@@ -208,7 +208,7 @@ apa_print.summary.ref.grid <- function(
   } else {
     colnames(contrast_table) <- c(split_by, "$\\Delta M$", conf_level, "$t$", "$df$", "$p$")
   }
-  
+
   apa_res$table <- contrast_table
 
   apa_res
