3df7a77c696f22b872a6ac516edcf384d85d0b30
Frederik Aust <frederikaust@Frederiks-MacBook-Pro.local>
2015-11-25 00:01:38 +0100

    Resolves merge conflict; adds new test for Mauchly tests.

--- tests/testthat/test_apa_print_htest.R
+++ tests/testthat/test_apa_print_htest.R
@@ -189,5 +189,17 @@ test_that(
   }
 )
 
+test_that(
+  "Mauchly test"
+  , {
+    mauchly_data <- data.frame(deg = gl(3, 1, 6, labels = c(0,4,8)), noise = gl(2, 3, 6, labels = c("A","P")))
 
+    mauchly_test <- mauchly.test(mlmfit, X = ~ deg + noise, idata = mauchly_data)
+    apa_print(mauchly_test)
+
+
+    mauchly_test <- mauchly.test(mlmfit, M = ~ deg + noise, X = ~ noise, idata = mauchly_data)
+    apa_print(mauchly_test)
+  }
+)
 
