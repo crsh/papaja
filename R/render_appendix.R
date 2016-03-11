4de706b7413ad2cfabc30229a05f2e48d11010fb
Frederik Aust <frederik.aust@uni-koeln.de>
2016-03-11 14:47:50 +0100

    render_appendix() adds appendix environment.

--- R/render_appendix.R
+++ R/render_appendix.R
@@ -13,12 +13,18 @@ render_appendix <- function(x) {
   target_format <- knitr::opts_knit$get("rmarkdown.pandoc.to")
 
   if(length(target_format) > 0 && target_format == "latex") {
+    # Create TeX-file
     tmp_name <- paste0(paste(sample(c(letters, LETTERS, 0:9), 28), collapse = ""), ".md")
-    knitr::knit(x, tmp_name)
+    knitr::knit(x, tmp_name, quiet = TRUE)
     new_name <- paste0(tools::file_path_sans_ext(x), ".tex")
     pandoc_call <- paste("pandoc -f markdown -t latex -p -o", shQuote(new_name), shQuote(tmp_name))
     system(pandoc_call)
     file.remove(tmp_name)
+
+    # Add appendix environment
+    tex <- readLines(new_name)
+    tex <- c("\\begin{appendix}", tex, "\\end{appendix}")
+    write(tex, file = new_name)
   }
 
   return(new_name)
