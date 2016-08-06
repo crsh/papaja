19174758bda8c19ace649e6ae3588240df5945bf
Frederik Aust <frederik.aust@uni-koeln.de>
2016-08-06 21:26:51 +0200

    Fix for Travis-CI integration

--- tests/testthat/test_meta.R
+++ tests/testthat/test_meta.R
@@ -6,8 +6,8 @@ test_that(
     lang <- papaja:::localize("english")
 
     expect_is(lang, "list")
-    expect_equal(length(lang), 7)
-    expect_equal(names(lang), c("author_note", "abstract", "keywords", "word_count", "table", "figure", "note"))
-    expect_equivalent(sapply(lang, class), rep("character", 7))
+    expect_equal(length(lang), 9)
+    expect_equal(names(lang), c("author_note", "abstract", "keywords", "word_count", "table", "figure", "note", "correspondence", "email"))
+    expect_equivalent(sapply(lang, class), rep("character", 9))
   }
 )
