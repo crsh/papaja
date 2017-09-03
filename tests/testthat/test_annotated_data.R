37dbf956076dd2b5db8e3624c488c01df75c861e
mariusbarth <marius.barth@uni-koeln.de>
2017-09-04 01:20:39 +0200

    changed a unit test to check consistency with currently installed release of R

--- tests/testthat/test_annotated_data.R
+++ tests/testthat/test_annotated_data.R
@@ -8,13 +8,15 @@ test_that(
     object_2 <- droplevels(object_1, exclude = "b")
     object_3 <- droplevels(object_1)
 
+    object_4 <- droplevels.factor(x = factor(letters[1:4], levels = letters[1:10]), exclude = "b")
+
     # test case with excluded levels
     expect_identical(
       object = object_2
       , expected = new(
         "annotated_factor"
         , c(1L, NA_integer_, 2:3)
-        , levels = letters[c(1, 3, 4)]
+        , levels = levels(object_4) # base behavior changed recently, so just check consistency with currently installed version
         , annotation = anno
         , label = "lab"
       )
