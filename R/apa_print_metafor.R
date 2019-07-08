f08d46a7c6483eb4021e504ba0fb9742de019ab6
ekothe <pc.miss@gmail.com>
2019-07-08 16:38:32 +0200

    first pass with apa_container

--- R/apa_print_metafor.R
+++ R/apa_print_metafor.R
@@ -6,36 +6,22 @@ apa_print.metafor <- function(
   , ...
 ){
 
-  validate(x, check_class = "rma")
+  #papaja:::validate(x, check_class = "rma.uni")
 
   est_name <- "b"
 
-#  if(is.matrix(ci)) {
-#    conf_level <- as.numeric(gsub("[^.|\\d]", "", colnames(ci), perl = TRUE))
-#    conf_level <- 100 - conf_level[1] * 2
-#  } else {
-#    conf_level <- 100 * ci
-#  }
+  apa_res <- papaja:::apa_print_container()
 
-  regression_table <- arrange_regression(
-    x
-    , est_name = est_name
-    , standardized = FALSE
-    , ci = ci
-    , ...
-  )
+  apa_res$estimate <- paste0("$b = ", x$b[1], "95\\% CI = $[", x$ci.lb, "$, $", x$ci.ub, "]$")
 
-  # Concatenate character strings and return as named list
-  apa_res <- apa_glm_res(regression_table, in_paren = in_paren, conf_level = conf_level)
-  names(apa_res$estimate) <- sanitize_terms(names(x$coefficients))
-  names(apa_res$statistic) <- names(apa_res$estimate)
-  names(apa_res$full_result) <- names(apa_res$estimate)
+  apa_res$statistic <- paste0("$z = ", x$zval, ", p = ", x$pval)
 
-  # Model fit
-  glance_x <- broom::glance(x)
-  apa_res$estimate$modelfit$aic <- paste0("$\\mathrm{AIC} = ", printnum(glance_x$AIC), "$")
-  apa_res$estimate$modelfit$bic <- paste0("$\\mathrm{BIC} = ", printnum(glance_x$BIC), "$")
+  apa_res$full_result <- paste(apa_res$estimate, apa_res$statistic)
 
   apa_res
 }
+
+
+
+
 
