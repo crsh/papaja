2be1ae41d59123a30eba8db23844e771ba673e50
mariusbarth <marius.barth@uni-koeln.de>
2019-08-28 00:12:05 +0200

    Add support for `manova` and `summary.manova` to `apa_print()`
    - add two `apa_print()` methods
    - return an informative error message if an object of class `Anova.mlm` from MANOVA is passed to `apa_print()`

---
+++ R/apa_print_manova.R
@@ -0,0 +1,115 @@
+
+#' @param test For MANOVA, the multivariate test statistic to be reported, see \code{\link{summary.manova}}.
+#' @rdname apa_print.aov
+#' @export
+
+apa_print.manova <- function(x, test = "Pillai", ...) {
+
+  tidied_x <- as.data.frame(
+    x = broom::tidy(x, test = test, ...)
+    , stringsAsFactors = FALSE
+  )
+  print_manova(x = tidied_x, ...)
+}
+
+
+#' @rdname apa_print.aov
+#' @export
+
+apa_print.summary.manova <- function(x, ...) {
+
+  tidied_x <- as.data.frame(
+    x = broom::tidy(x, ...)
+    , stringsAsFactors = FALSE
+  )
+  print_manova(x = tidied_x, ...)
+}
+
+
+
+#' @keywords internal
+
+print_manova <- function(
+  x
+  , in_paren = FALSE
+) {
+
+
+
+  estimate_names <- c(
+    pillai = "V"
+    , wilks = "\\Lambda"
+    , hl = "T"
+    , roy = "\\theta"
+  )
+
+
+  estimate_names_with_dollars <- paste0("$", estimate_names, "$")
+  names(estimate_names_with_dollars) <- names(estimate_names)
+
+  multivariate_stat <- intersect(names(estimate_names), colnames(x))
+
+  if(length(multivariate_stat) == 0L) {
+    stop("No multivariate test statistic found in results object.")
+  } # move this to input validation
+
+  col_names <- c(
+    term = "Effect"
+    , df = "df"
+    , pillai = "pillai"
+    , wilks = "wilks"
+    , hl = "hl"
+    , roy = "roy"
+    , statistic = "F"
+    , num.df ="df1"
+    , den.df = "df2"
+    , p.value ="p"
+  )
+  colnames(x) <- col_names[colnames(x)]
+
+  var_labels <- c(
+    estimate_names_with_dollars
+    , "Effect" = "Effect"
+    , "F" = "$F$"
+    , "df1" = "$\\mathit{df}_1$"
+    , "df2" = "$\\mathit{df}_2$"
+    , "p" = "$p$"
+  )
+
+  x <- x[x$Effect != "Residuals", colnames(x) != "df", drop = FALSE]
+
+  x[["Effect"]] <- prettify_terms(x[["Effect"]])
+  x[[multivariate_stat]] <- printnum(x[[multivariate_stat]])
+  x[["F"]] <- printnum(x[["F"]])
+  x[["df1"]] <-as.character(round(x[["df1"]], digits = 2))
+  x[["df2"]] <-as.character(round(x[["df2"]], digits = 2))
+  x[["p"]] <- printp(x[["p"]])
+
+  x <- sort_terms(x, "Effect")
+
+  variable_labels(x) <- var_labels[colnames(x)]
+
+  # assemble final output object
+  res <- apa_print_container()
+
+  res$statistic <- as.list(apply(X = x, MARGIN = 1, function(y) {
+    stat <- paste0(
+      "$", estimate_names[[multivariate_stat]], " = ", y[[multivariate_stat]], "$, "
+      , "$F(", y[["df1"]], ", ", y[["df2"]], ") = ", y[["F"]], "$, "
+      , "$p ", add_equals(y[["p"]]), "$"
+    )
+    stat
+  }))
+  if(in_paren) res$statistic <- in_paren(res$statistic)
+  names(res$statistic) <- gsub(x$Effect, pattern = " $\\times$ ", replacement = "_", fixed = TRUE)
+
+  res$full_result <- res$statistic
+
+  res$table <- x
+  class(res$table) <- c("apa_results_table", "data.frame")
+  res
+}
+
+
+
+
---
+++ tests/testthat/test_apa_print_manova.R
@@ -0,0 +1,76 @@
+
+context("apa_print() for MANOVA")
+
+test_that(
+  "Two-way MANOVA: apa_print.manova and apa_print.summary.manova"
+  , {
+    set.seed(42)
+
+    data <- npk
+    data$yield2 <- as.integer(data$N) * 2 + rnorm(n = nrow(data), sd = 2)
+
+    x1 <- manova(formula = cbind(yield, yield2) ~ N * P, data = data)
+    x2 <- summary(x1, test = "Wilks")
+
+    out1 <- apa_print(x1) # Pillai
+    out2 <- apa_print(x2) # Wilks
+    out3 <- apa_print(x1, test = "H") # Hotelling-Lawley, pmatch
+    out4 <- apa_print(x1, test = "R", in_paren = TRUE) # Roy's root, pmatch
+
+    expect_identical(
+      object = out1$statistic$N
+      , expected = "$V = 0.25$, $F(2, 19) = 3.22$, $p = .062$"
+    )
+    expect_identical(
+      object = out2$full_result$N_P
+      , expected = "$\\Lambda = 0.96$, $F(2, 19) = 0.37$, $p = .697$"
+    )
+    expect_identical(
+      object = out3$full_result$P
+      , expected = "$T = 0.02$, $F(2, 19) = 0.20$, $p = .821$"
+    )
+
+    # in_paren
+    expect_identical(
+      object = out4$statistic$N
+      , expected = "$\\theta = 0.34$, $F[2, 19] = 3.22$, $p = .062$"
+    )
+
+    # apa_results_table
+    expect_identical(
+      object = variable_labels(out3$table)
+      , expected = list(
+        Effect = "Effect"
+        , hl = "$T$"
+        , `F` = "$F$"
+        , df1 = "$\\mathit{df}_1$"
+        , df2 = "$\\mathit{df}_2$"
+        , p = "$p$"
+      )
+    )
+    expect_identical(
+      object = class(out1$table)
+      , expected = c("apa_results_table", "data.frame")
+    )
+  }
+)
+
+test_that(
+  "Anova.mlm from MANOVA not supported"
+  , {
+    set.seed(42)
+
+    data <- npk
+    data$yield2 <- as.integer(data$N) * 2 + rnorm(n = nrow(data), sd = 2)
+
+    expect_error(
+      apa_print(car::Manova(manova(formula = cbind(yield, yield2) ~ N * P, data = data), type = 3))
+      , fixed = TRUE
+      , regexp = "Anova.mlm objects from car::Manova are not supported, yet. You can try using stats::manova instead if Type I oder Type II sums of squares are adequate for your analysis."
+    )
+  }
+)
+
+
+
+
