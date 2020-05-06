b30363ec7eaf34aeb1a963997e474346fb90527c
Frederik Aust <frederik.aust@uni-koeln.de>
2020-05-06 17:43:30 +0200

    Fix apa_print()-emmeans error for intercept-only reference grid.

--- R/apa_print_emm_lsm.R
+++ R/apa_print_emm_lsm.R
@@ -103,7 +103,7 @@ apa_print.summary_emm <- function(
 
 
   # Assamble table
-  contrast_table <- data.frame(x)
+  contrast_table <- data.frame(x, check.names = FALSE)
   contrast_table[, factors] <- printnum(contrast_table[, factors])
 
   if("null" %in% colnames(contrast_table)) { # This could go in a table note
