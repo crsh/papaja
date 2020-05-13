c877110b4e96df85f30760a10c40a9e20a826812
Frederik Aust <frederik.aust@uni-koeln.de>
2020-05-13 23:18:48 +0200

    Improves robustness of docx_fixes.lua. Resolves #365

--- inst/rmd/docx_fixes.lua
+++ inst/rmd/docx_fixes.lua
@@ -11,8 +11,9 @@ function Header (elem)
 end
 
 function Image (img)
-  img.caption[1] = pandoc.Emph(img.caption[1])
-  img.caption[3] = pandoc.Emph(img.caption[3])
-  img.caption[4] = pandoc.Str(".  ")
+  if img.caption[1] ~= nil then
+    img.caption[1] = pandoc.Emph(img.caption[1])
+    img.caption[3] = pandoc.Emph(string.gsub(img.caption[3].c, ":", ".  "))
+  end
   return img
 end
