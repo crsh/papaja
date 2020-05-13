68fbc60ea3eb75359a7b597527bcadf4c987517d
mariusbarth <marius.barth@uni-koeln.de>
2020-05-13 19:00:14 +0200

    removed methods for HLMs and roxygenized

--- R/apa_results_table.R
+++ R/apa_results_table.R
@@ -50,13 +50,24 @@ print.apa_results_table <- function(x, ...) {
 }
 
 
-# #' @export
-
-`$.apa_results_table` <- function(x, name) {
-  if(name == "F") {
-    message("Indexing an apa_results_table with `$F` is deprecated. Use `$statistic` instead.")
-    return(x$statistic)
-  } else {
-    x[[name]]
-  }
-}
+#' #' @export
+#'
+#' `$.apa_results_table` <- function(x, name) {
+#'
+#'   aliases <- list(
+#'     "F" = "statistic"
+#'     # , "p" = "p.value" # no, because partical matching solves this issue
+#'     , "predictor" = "term"
+#'   )
+#'   pmatch(name, c(names(aliases), colnames(x)))
+#'
+#'   if(name %in% names(aliases)) {
+#'     message("Indexing an apa_results_table with `$"
+#'     , name
+#'     , "` is deprecated. Use `$"
+#'     , aliases[name]
+#'     , "` instead.")
+#'     name <- aliases[name]
+#'   }
+#'   NextMethod("$", object = x, name = name) # dispatch to data.frame method
+#' }
