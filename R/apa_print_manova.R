0a322817f6c0d13b18229faffc0232cd3fce7a04
mariusbarth <marius.barth@uni-koeln.de>
2020-05-26 18:06:10 +0200

    move MANOVA support to new workflow

--- R/apa_print_manova.R
+++ R/apa_print_manova.R
@@ -6,26 +6,49 @@
 
 apa_print.manova <- function(x, test = "Pillai", ...) {
 
-  tidied_x <- as.data.frame(
-    x = broom::tidy(x, test = test, ...)
-    , stringsAsFactors = FALSE
-  )
-  print_manova(x = tidied_x, ...)
+  ellipsis <- list(...)
+  ellipsis$x <- summary(x, test = test)
+  # tidied_x <- as.data.frame(
+  #   x = broom::tidy(x, test = test, ...)
+  #   , stringsAsFactors = FALSE
+  # )
+  # print_manova(x = tidied_x, ...)
+  do.call("apa_print", ellipsis)
 }
 
 
-# #' @rdname apa_print.aov
-# #' @method apa_print summary.manova
-# #' @export
-#
-# apa_print.summary.manova <- function(x, ...) {
-#
-#   tidied_x <- as.data.frame(
-#     x = broom::tidy(x, ...)
-#     , stringsAsFactors = FALSE
-#   )
-#   print_manova(x = tidied_x, ...)
-# }
+#' @rdname apa_print.aov
+#' @method apa_print summary.manova
+#' @export
+
+apa_print.summary.manova <- function(x, ...) {
+
+  ellipsis <- defaults(
+    list(...)
+    , set.if.null = list(
+      in_paren = FALSE
+    )
+  )
+
+  resid_row <- apply(X = x$stats, MARGIN = 1L, anyNA)
+
+  df <- data.frame(
+    x$stats[!resid_row, , drop = FALSE]
+    , stringsAsFactors = FALSE
+  )
+  df$Effect <- rownames(df)
+  # df$multivariate.df1 <- df$Df
+  # df$multivariate.df2 <- x$stats[resid_row, "Df"]
+  df$Df <- NULL
+
+  sanitized <- sanitize_table(df)
+  prettified <- print_table(sanitized)
+  create_container(
+    prettified
+    , sanitized_terms = sanitize_terms(df$Effect)
+    , in_paren = ellipsis$in_paren
+  )
+}
 
 
 
