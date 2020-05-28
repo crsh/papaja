bfec61b2603dab9eea71d02d30e189154561d4b6
mariusbarth <marius.barth@uni-koeln.de>
2020-05-28 14:54:31 +0200

    test apa_print() for model comparisons

--- tests/testthat/test_apa_print_model_comp.R
+++ tests/testthat/test_apa_print_model_comp.R
@@ -11,8 +11,7 @@ test_that(
     # No bootstrapped Delta R^2 CI
     model_comp <- apa_print(list(Baseline = mod1, Length = mod2, Both = mod3), boot_samples = 0)
 
-    expect_is(model_comp, "list")
-    expect_equal(names(model_comp), container_names)
+    expect_apa_results(model_comp)
 
     # stat
     expect_is(model_comp$stat, "list")
@@ -44,17 +43,28 @@ test_that(
     # table
     correct_table <- structure(
       list(
-        Baseline = c("$6.53$ $[5.58$, $7.47]$", "$-0.22$ $[-0.53$, $0.08]$", "", "", "$.01$ $[0.00$, $0.06]$", "2.07", "1", "148", ".152", "371.99", "381.02", "", "", "", "", "", "", "")
-        , Length = c("$2.25$ $[1.76$, $2.74]$", "$0.60$ $[0.46$, $0.73]$", "$0.47$ $[0.44$, $0.51]$", "", "$.84$ $[0.79$, $0.87]$", "386.39", "2", "147", "< .001", "101.03", "113.07", "$.83$", "853.31", "1", "148", "< .001", "-270.97", "-267.96")
-        , Both = c("$1.86$ $[1.36$, $2.35]$", "$0.65$ $[0.52$, $0.78]$", "$0.71$ $[0.60$, $0.82]$", "$-0.56$ $[-0.81$, $-0.30]$", "$.86$ $[0.82$, $0.89]$", "295.54", "3", "146", "< .001", "84.64", "99.70", "$.02$", "19.04", "1", "148", "< .001", "-16.38", "-13.37")
+        Baseline = structure(
+          c("$6.53$ $[5.58$, $7.47]$", "$-0.22$ $[-0.53$, $0.08]$", "", "", "$.01$ $[0.00$, $0.06]$", "2.07", "1", "148", ".152", "371.99", "381.02", "", "", "", "", "", "", "")
+          , label = "Baseline"
+          , class = c("papaja_labelled", "character")
+        )
+        , Length = structure(
+          c("$2.25$ $[1.76$, $2.74]$", "$0.60$ $[0.46$, $0.73]$", "$0.47$ $[0.44$, $0.51]$", "", "$.84$ $[0.79$, $0.87]$", "386.39", "2", "147", "< .001", "101.03", "113.07", "$.83$", "853.31", "1", "148", "< .001", "-270.97", "-267.96")
+          , label = "Length"
+          , class = c("papaja_labelled", "character")
+        )
+        , Both = structure(
+          c("$1.86$ $[1.36$, $2.35]$", "$0.65$ $[0.52$, $0.78]$", "$0.71$ $[0.60$, $0.82]$", "$-0.56$ $[-0.81$, $-0.30]$", "$.86$ $[0.82$, $0.89]$", "295.54", "3", "146", "< .001", "84.64", "99.70", "$.02$", "19.04", "1", "148", "< .001", "-16.38", "-13.37")
+          , label = "Both"
+          , class = c("papaja_labelled", "character")
+        )
       )
       , .Names = c("Baseline", "Length", "Both")
       , row.names = c("Intercept", "Sepal Width", "Petal Length", "Petal Width", "$R^2$ [90\\% CI]", "$F$", "$df_1$", "$df_2$", "$p$", "$\\mathrm{AIC}$", "$\\mathrm{BIC}$", "$\\Delta R^2$", "$F$ ", "$df_1$ ", "$df_2$ ", "$p$ ", "$\\Delta \\mathrm{AIC}$", "$\\Delta \\mathrm{BIC}$")
       , class = c("apa_results_table", "data.frame")
     )
 
-    expect_is(model_comp$table, "data.frame")
-    expect_equal(colnames(model_comp$table), c("Baseline", "Length", "Both"))
+    expect_identical(colnames(model_comp$table), c("Baseline", "Length", "Both"))
     expect_identical(model_comp$table$Baseline, correct_table$Baseline)
     expect_identical(model_comp$table$Length, correct_table$Length)
     expect_identical(model_comp$table$Both, correct_table$Both)
@@ -62,6 +72,7 @@ test_that(
 
     # Test omission of incomplete model names & CI
     incomplete_names <- apa_print(list(mod1, Length = mod2, Both = mod3), boot_samples = 0)
+    expect_apa_results(incomplete_names)
 
     ## stat
     expect_is(incomplete_names$stat, "list")
@@ -76,10 +87,10 @@ test_that(
     expect_equal(names(incomplete_names$full), c("model2", "model3"))
 
     ## table
-    expect_is(incomplete_names$table, "data.frame")
     expect_equal(colnames(incomplete_names$table), paste("Model", 1:3))
 
     incomplete_names2 <- apa_print(list(mod1, mod2, mod3), boot_samples = 0)
+    expect_apa_results(incomplete_names2)
     expect_identical(incomplete_names, incomplete_names2)
 
 
@@ -88,12 +99,14 @@ test_that(
 
     set.seed(1337)
     model_comp_boot <- apa_print(list(Baseline = mod1, Length = mod2, Both = mod3), boot_samples = 1e3)
+    expect_apa_results(model_comp_boot)
 
     expect_equal(model_comp_boot$est$Length, "$\\Delta R^2 = .83$, 90\\% CI $[.76$, $.86]$")
 
     expect_equal(model_comp_boot$est$Both, "$\\Delta R^2 = .02$, 90\\% CI $[.01$, $.04]$")
 
     model_comp_boot2 <- apa_print(list(Baseline = mod1, Length = mod2), boot_samples = 1e3, ci = 0.5)
+    expect_apa_results(model_comp_boot2)
 
     expect_equal(model_comp_boot2$est$Length, "$\\Delta R^2 = .83$, 50\\% CI $[.80$, $.84]$")
   }
