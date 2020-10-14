5f1dcde01158e94eeecafa34b3fce77655cddcb2
mariusbarth <marius.barth@uni-koeln.de>
2020-10-14 13:56:47 +0200

    updated apa6_pdf() tests

--- tests/testthat/test_apa6_formats.R
+++ tests/testthat/test_apa6_formats.R
@@ -5,24 +5,12 @@ test_that(
     apa6_format <- apa6_pdf()
 
     expect_s3_class(apa6_format, "rmarkdown_output_format", exact = TRUE)
-    expect_identical(
-      apa6_format$knitr$opts_chunk
-      , expected = list(
-        dev = c("pdf", "png")
-        , fig.width = 6.5
-        , fig.height = 4.5
-        , dev.args = list(
-          pdf = list(
-            useDingbats = FALSE
-          )
-        )
-        , crop = TRUE
-        , echo = FALSE
-        , message = FALSE
-        , fig.cap = " "
-        , dpi = 300
-      )
-    )
+    expect_identical(apa6_format$knitr$opts_chunk$echo, FALSE)
+    expect_identical(apa6_format$knitr$opts_chunk$message, FALSE)
+    expect_identical(apa6_format$knitr$opts_chunk$fig.cap, " ")
+    expect_identical(apa6_format$knitr$opts_knit$rmarkdown.pandoc.to, "latex")
+    expect_identical(apa6_format$knitr$opts_chunk$dev, c("pdf", "png"))
+    expect_identical(apa6_format$knitr$opts_chunk$dpi, 300)
   }
 )
 
